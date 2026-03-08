bb00380a82e9fc8a93ec525c8ef5a7df48dad637
yangyuxiang <laoerbusishiweiyao@gmail.com>
2026-03-08 14:54:37 +0800

    refactor(runtime): 统一操作码数值格式

--- Web/unity-web-ui/src/runtime/Opcode.ts
+++ Web/unity-web-ui/src/runtime/Opcode.ts
@@ -1,9 +1,9 @@
 export const Opcode = {
-    Web: 1002,
-    WebLoaded: 100201,
-    WebTouchData: 100202,
-    WebPointerData: 100203,
-    WebMouseData: 100204,
+    Web: 102,
+    WebLoaded: 10201,
+    WebTouchData: 10202,
+    WebPointerData: 10203,
+    WebMouseData: 10204,
 } as const;
 
 export type TouchPhase = typeof Opcode[keyof typeof Opcode];
